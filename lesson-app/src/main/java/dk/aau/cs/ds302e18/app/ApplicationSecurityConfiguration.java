72dc2ff4c714b1c390e46e906eaa601fc6dd85a8
Unknown <seraph14@hotmail.com>
2018-11-05 17:06:44 +0100

    Added GDPR page along with GDPR pop-up
    TODO: Make it possible for client to opt out of cookies we eventually use

--- lesson-app/src/main/java/dk/aau/cs/ds302e18/app/ApplicationSecurityConfiguration.java
+++ lesson-app/src/main/java/dk/aau/cs/ds302e18/app/ApplicationSecurityConfiguration.java
@@ -49,7 +49,7 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/css/*", "/js/*", "/register", "/test").permitAll()
+                .antMatchers("/", "/index", "/css/*", "/js/*", "/register", "/test", "/gdpr").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
