3348cc13fb514b54ba95208f3a28932b6c70769a
Samuel <sava17@student.aau.dk>
2018-11-09 01:06:45 +0100

    Coursecontroller can finally add lesson depending on weekdays! In a way where input of weekdays makes sense.  Also added functionality to add more than one lesson pr. day

---
+++ lesson-app/src/main/java/dk/aau/cs/ds302e18/app/auth/CourseController.java
@@ -0,0 +1,191 @@
+package dk.aau.cs.ds302e18.app.auth;
+
+import dk.aau.cs.ds302e18.app.DBConnector;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class CourseController {
+    private Connection conn;
+
+    public CourseController() {
+        this.conn = new DBConnector().createConnectionObject();
+    }
+
+    public String saveUsernamesAsString(ArrayList<User> userList) {
+        String combinedString = "";
+        for (User user : userList) {
+            combinedString += user.getUsername() + ":";
+        }
+        return combinedString;
+    }
+
+    /* Separates a string into usernames and creates an list of users with those usernames. */
+    public ArrayList<User> saveStringAsUsers(String studentUsernames) {
+        ArrayList<User> userList = new ArrayList<>();
+        String[] parts = studentUsernames.split(":");
+        for (int i = 0; i < parts.length; i++) {
+            userList.add(new User());
+            userList.get(i).setUsername(parts[i]);
+        }
+        return userList;
+    }
+
+
+    public void createEmptyCourse(int courseID) {
+        try {
+            /* Creates a mysql database connection */
+            Statement st = conn.createStatement();
+            st.executeUpdate("INSERT INTO `course`(`course_id`, `student_usernames`) VALUES (" + courseID + ",'')");
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Takes an list of student users that will initially be assigned to the course as input */
+    public void createEmptyCourse(int courseID, ArrayList<User> studentList) {
+        try {
+            Statement st = conn.createStatement();
+            String usernamesAsString = saveUsernamesAsString(studentList);
+            st.executeUpdate("INSERT INTO `course`(`course_id`, `student_usernames`) VALUES (" + courseID + ",'" + usernamesAsString + "')");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void addStudent(int courseID, User studentUser) {
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT `student_usernames` FROM `course` WHERE `course_id` = " + courseID + "");
+            rs.next();
+            /* Finds the current list of students */
+            String usernames = rs.getString("student_usernames");
+            /* Adds the new student */
+            usernames += studentUser.getUsername() + ":";
+            st.executeUpdate("UPDATE `course` SET`student_usernames`='" + usernames + "' WHERE `course_id` = '" + courseID + "'");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeStudent(int courseID, User studentUser) {
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT `student_usernames` FROM `course` WHERE `course_id` = " + courseID + "");
+            rs.next();
+            /* Finds the current list of students */
+            String usernames = rs.getString("student_usernames");
+            /* Removes the targeted student */
+            String usernamesWithoutStudent = usernames.replace(studentUser.getUsername() + ":", "");
+            st.executeUpdate("UPDATE `course` SET`student_usernames`='" + usernamesWithoutStudent + "' WHERE `course_id` = '" + courseID + "'");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteCourse(int courseID) {
+        try {
+            Statement st = conn.createStatement();
+            st.executeUpdate("DELETE FROM `course` WHERE `course_id` = '" + courseID + "'");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Get list of students from course database */
+    /* Prepare lesson date, lesson ID, instructor, location, type. DONE */
+    /* needs to sort lessons after ID, and pick the ID incremented by one. DONE */
+    /* Still needs number of hours at a time and start date should be the exact time the first lesson starts */
+    /* every lesson needs an associated courseID if there are any */
+    public void courseAddLessons(Date startDate, ArrayList<Integer> lessonPlacementsFromOffset, int numberLessons, int numberLessonsADay,
+                                 ArrayList<User> userList, int courseID,
+                                 String location, String instructorName, int lessonType) {
+
+        String usernamesString = saveUsernamesAsString(userList);
+        ArrayList<Date> lessonDates = createLessonDates(startDate, lessonPlacementsFromOffset, numberLessons, numberLessonsADay);
+        /* All added lessons will be initialized as active */
+        int lessonState = 1;
+        int lastEnteredLessonID;
+        try {
+            Statement st = conn.createStatement();
+            /* Gets the ID of the last lesson created, which will be the first ID of the created course lessons */
+            ResultSet rs = st.executeQuery("SELECT `lesson_id` FROM `lesson`");
+            rs.last();
+            lastEnteredLessonID = rs.getInt("lesson_id");
+
+            for(int j = 0; j<lessonDates.size(); j++) {
+                int lessonID = lastEnteredLessonID + 1 + j;
+                String lessonDate = reformatDate(lessonDates.get(j));
+                st.executeUpdate(   "INSERT INTO `lesson`(`lesson_id`, `state`, `lesson_date`, `instructor`, " +
+                        "`lesson_location`, `lesson_type`, `student_list`, `course_id`) " + "VALUES (" + lessonID + ","+ lessonState +",'"+ lessonDate +"','"+ instructorName +"','"+
+                        location + "',"+ lessonType +",'"+ usernamesString +"',"+ courseID +")");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Creates an ArrayList of dates with the dates a course should contain. The dates are set to start at startDate
+       and lessons will be distributed in the specified weekdays. Sunday is 0, monday is 1 and so on.
+       */
+
+    public ArrayList<Date> createLessonDates(Date startDate, ArrayList<Integer> weekdays, int numberLessonsToDistribute,
+                                             int numberLessonsADay) {
+        ArrayList<Date> lessonDates = new ArrayList<>();
+        Date currentDayDate = startDate;
+        int dayCount = 0;
+        /* A lesson should minimum be 45 minutes according to law, and the two interviewed driving schools had a 45
+           minute lesson duration. */
+        int lessonDurationMinutes = 45;
+
+        int oneMinuteInMilliseconds = 60000;
+        int lessonDurationMilliseconds = oneMinuteInMilliseconds * lessonDurationMinutes;
+
+        /* While all lessons has not yet been distributed */
+        while (numberLessonsToDistribute > 0) {
+            /* 86400000 * dayCount is not contained in a variable due to the possible of reaching an overflow of most
+               data-types. Date is by default suitable to handle very large numbers. */
+            currentDayDate = new Date(startDate.getTime() + 86400000 * dayCount);
+            if(weekdays.contains(currentDayDate.getDay())) {
+                /* If it is one of the weekdays specified in the weekdays array, add an number of lessons equal to
+                * number lessons a day */
+                for(int g = 0; g<numberLessonsADay; g++){
+                    System.out.println(g);
+                    lessonDates.add(new Date(currentDayDate.getTime() + lessonDurationMilliseconds * g));
+                    numberLessonsToDistribute--;
+                }
+            }
+            //System.out.println(dayCount);
+            dayCount += 1;
+        }
+        return lessonDates;
+    }
+
+    //Reformats the Java Date format to the mySQL datetime format and returns it in a String.
+    //This functions works based on the Java Date format for generating a Date.
+    //This means that this function counterworks the addition of +1900 in allocating a new Date.
+    //Therefor any pre-formatted dates won't work with this function.
+    private String reformatDate(Date date) {
+        if(date == null) {
+            try {
+                throw new Exception("Invalid date.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        String formattetDate = (date.getYear() + 1900) + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+                + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        return formattetDate;
+    }
+}
--- lesson-app/src/test/java/dk/aau/cs/ds302e18/app/auth/CourseControllerTest.java
+++ lesson-app/src/test/java/dk/aau/cs/ds302e18/app/auth/CourseControllerTest.java
@@ -1,16 +1,20 @@
 package dk.aau.cs.ds302e18.app.auth;
 
-import dk.aau.cs.ds302e18.app.CourseController;
+import dk.aau.cs.ds302e18.app.auth.CourseController;
+import dk.aau.cs.ds302e18.app.auth.User;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CourseControllerTest {
     private ArrayList<User> userList;
     private CourseController courseController;
+    private Date startDate;
 
     @Before
     public void setUp() throws Exception {
@@ -28,6 +32,7 @@ public class CourseControllerTest {
         userList.add(user1);
         userList.add(user2);
         userList.add(user3);
+        startDate = new Date(2018,11,1,16,0);
     }
 
     @Test
@@ -69,5 +74,36 @@ public class CourseControllerTest {
     @Test
     public void deleteCourse() {
         courseController.deleteCourse(3);
+    }
+
+    @Test
+    public void createLessonDates() {
+        ArrayList<Integer> lessonPlacementsFromOffset = new ArrayList<>();
+        lessonPlacementsFromOffset.add(0);
+        lessonPlacementsFromOffset.add(1);
+        lessonPlacementsFromOffset.add(6);
+        /* An uneven amount of lessons also checks that the if statement in the for loop prevents the loop from creating
+           unnecessary extra lessons. */
+        ArrayList<Date> dateList = courseController.createLessonDates(startDate, lessonPlacementsFromOffset,5,2);
+        for(Date date: dateList) {
+            System.out.println("lesson date:" + date.getDate() + " weekday: " + date.getDay());
+        }
+    }
+
+    @Test
+    public void courseAddLessons() {
+        ArrayList<Integer> weekdays = new ArrayList<>();
+        weekdays.add(0);
+        weekdays.add(1);
+        courseController.courseAddLessons(startDate,weekdays,3, 2, userList, 7,"AAU", "Bin Yang", 1);
+    }
+
+    @Test
+    public void testCalendar() {
+        Date testDate = new Date();
+        Date newDate = new Date();
+        Calendar test = Calendar.getInstance();
+        System.out.println(test.getTime().getDay());
+        int value = 0;
     }
 }
