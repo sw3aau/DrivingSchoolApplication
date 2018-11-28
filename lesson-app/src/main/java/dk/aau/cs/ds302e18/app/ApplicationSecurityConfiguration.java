ea89e136951594f9a195ff36f53afbf06f5d6d2e
Unknown <seraph14@hotmail.com>
2018-11-28 12:36:35 +0100

    Removed "USER" as default authority

--- lesson-app/src/main/java/dk/aau/cs/ds302e18/app/ApplicationSecurityConfiguration.java
+++ lesson-app/src/main/java/dk/aau/cs/ds302e18/app/ApplicationSecurityConfiguration.java
@@ -36,7 +36,6 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     public GrantedAuthoritiesMapper authoritiesMapper(){
         SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
         authorityMapper.setConvertToUpperCase(true);
-        authorityMapper.setDefaultAuthority("USER");
         return authorityMapper;
     }
 
